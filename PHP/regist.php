f0eebf206cc5ce4f7e54f0fd42210e84a9a3f86b
Flokess <you@example.com>
2023-11-13 15:54:08 +0300

    registr

---
+++ PHP/regist.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$UserName=$_POST['UserName'];
+$UserLastName=$_POST['UserLastName'];
+$UserPhone=$_POST['UserPhone'];
+$UserEmail=$_POST['UserEmail'];
+$UserPassword=$_POST['UserPassword'];
+$BirthDate=$_POST['BirthDate'];
+
+var_dump($_POST);
