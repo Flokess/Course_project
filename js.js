44ed98c8408d8d4711766b96d705583abdc9ce51
Flokess <you@example.com>
2023-11-28 16:00:06 +0300

    inalyewies

---
+++ js.js
@@ -0,0 +1,45 @@
+
+const submenus = document.querySelectorAll('.submenu');
+
+// Add event listeners to each submenu
+submenus.forEach(submenu => {
+        const parentMenuItem = submenu.parentNode;
+
+        // Show submenu on mouse enter
+        parentMenuItem.addEventListener('mouseenter', () => {
+                submenu.style.display = 'block';
+        });
+
+        // Hide submenu on mouse leave
+        parentMenuItem.addEventListener('mouseleave', () => {
+                submenu.style.display = 'none';
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+        const writeReviewButton = document.getElementById('writeReviewButton');
+        const reviewForm = document.getElementById('reviewForm');
+
+        writeReviewButton.addEventListener('click', function() {
+                reviewForm.style.display = (reviewForm.style.display === 'none' || reviewForm.style.display === '') ? 'block' : 'none';
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+        const stars = document.querySelectorAll('.star');
+        const rating = document.getElementById('ratingStars');
+
+        stars.forEach(star => {
+                star.addEventListener('click', function() {
+                        const value = this.getAttribute('data-value');
+                        rating.setAttribute('data-rating', value);
+                        stars.forEach(s => {
+                                if (s.getAttribute('data-value') <= value) {
+                                        s.classList.add('active');
+                                } else {
+                                        s.classList.remove('active');
+                                }
+                        });
+                });
+        });
+});
